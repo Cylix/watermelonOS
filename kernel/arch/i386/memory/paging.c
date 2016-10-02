5db2c8c29dae530e2a4f0183c54128023aa089f0
Simon Ninon <simon.ninon@gmail.com>
2016-10-02 20:18:25 +0200

    enhancement of paging intialization by initializing all possible page table (and identity map their addresses)

--- kernel/arch/i386/memory/paging.c
+++ kernel/arch/i386/memory/paging.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 page_dir_t paging_page_directory = NULL;
-page_tab_t paging_system_page_table = NULL;
+page_dir_t paging_page_tables[PAGING_PAGE_DIR_NB_ENTRIES] __attribute__((aligned(FRAME_ALLOC_FRAME_SIZE)));
 
 unsigned int get_dir();
 
@@ -21,32 +21,39 @@ paging_init(void) {
     return -1;
   }
 
-  //! initialize the table with empty entries
-  for (unsigned int i = 0; i < PAGING_PAGE_DIR_NB_ENTRIES; ++i)
-    paging_page_directory[i] = PAGING_PAGE_DIR_EMPTY_ENTRY;
+  //! initialize the directory with newly allocated page tables
+  for (unsigned int i = 0; i < PAGING_PAGE_DIR_NB_ENTRIES; ++i) {
+    paging_page_tables[i] = frame_allocator_alloc();
 
-  //! First page table, providing identity mapping for kernel space
-  paging_system_page_table = frame_allocator_alloc();
+    if (paging_page_tables[i] == NULL) {
+      printf("[ERR ] Can't allocate frame for page directory entry #%d.\n", i);
+      return -1;
+    }
 
-  if (paging_system_page_table == NULL) {
-    printf("[ERR ] Can't allocate frame for page directory enry #0.\n");
-    return -1;
+    //! Store mapping as RD/Supervisor/Present
+    paging_page_directory[i] = (uint32_t)paging_page_tables[i] | PAGING_PAGE_FLAG_RDWR | PAGING_PAGE_FLAG_PRESENT;
   }
 
   //! Identity mapping of kernel space
   //! Avoid identity mapping addresses located after the kernel space
-  for (unsigned int i = 0; i < PAGING_PAGE_TAB_NB_ENTRIES; ++i) {
-    uint32_t addr = i * FRAME_ALLOC_FRAME_SIZE;
-    if ((void*)addr < end_kernel_addr)
-      paging_system_page_table[i] = addr | PAGING_PAGE_FLAG_RDWR | PAGING_PAGE_FLAG_PRESENT;
-    else
-      paging_system_page_table[i] = PAGING_PAGE_TAB_EMPTY_ENTRY;
-  }
-  //! Identity map the system page table
-  paging_system_page_table[(uint32_t)paging_system_page_table / 4096] = (uint32_t)paging_system_page_table | PAGING_PAGE_FLAG_RDWR | PAGING_PAGE_FLAG_PRESENT;
+  for (unsigned int i = 0; i < PAGING_PAGE_DIR_NB_ENTRIES; ++i) {
+    for (unsigned int j = 0; j < PAGING_PAGE_TAB_NB_ENTRIES; ++j) {
+      uint32_t addr = i * PAGING_PAGE_TAB_NB_ENTRIES * FRAME_ALLOC_FRAME_SIZE + j * FRAME_ALLOC_FRAME_SIZE;
 
-  //! Store mapping as RD/Supervisor/Present
-  paging_page_directory[0] = (unsigned int)paging_system_page_table | PAGING_PAGE_FLAG_RDWR | PAGING_PAGE_FLAG_PRESENT;
+      if ((void*)addr < end_kernel_addr)
+        paging_page_tables[i][j] = addr | PAGING_PAGE_FLAG_RDWR | PAGING_PAGE_FLAG_PRESENT;
+      else
+        paging_page_tables[i][j] = PAGING_PAGE_TAB_EMPTY_ENTRY;
+    }
+  }
+
+  //! Identity map the system page table
+  for (unsigned int i = 0; i < PAGING_PAGE_DIR_NB_ENTRIES; ++i) {
+    unsigned int page_dir_idx = (uint32_t)paging_page_tables[i] / (PAGING_PAGE_TAB_NB_ENTRIES * FRAME_ALLOC_FRAME_SIZE);
+    unsigned int page_tab_idx = (uint32_t)paging_page_tables[i] % (PAGING_PAGE_TAB_NB_ENTRIES * FRAME_ALLOC_FRAME_SIZE) / FRAME_ALLOC_FRAME_SIZE;
+
+    paging_page_tables[page_dir_idx][page_tab_idx] = (uint32_t)paging_page_tables[i] | PAGING_PAGE_FLAG_RDWR | PAGING_PAGE_FLAG_PRESENT;
+  }
 
   //! load the directory and enable paging
   paging_load_page_directory(paging_page_directory);
